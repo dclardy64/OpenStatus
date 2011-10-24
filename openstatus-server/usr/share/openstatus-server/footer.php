e87360b8232460629617ba295d3344f0086b455e
Nick Moeck <nick@nickmoeck.com>
2011-10-24 08:23:11 +0000

    Fixing structure

--- openstatus-server/usr/share/openstatus-server/footer.php
+++
@@ -1,9 +0,0 @@
-<?php $end = microtime(); ?>
-                        <div id="footer" class="clear">
-                                <p>Page generated in <?php echo round(($end - $start), 5); ?>s.</p>
-                                <p><a href="http://www.nickmoeck.com/openstatus/">OpenStatus</a> v0.1.0 - based on <a href="https://github.com/DimeCadmium/scrd/">scrd</a> and <a href="https://github.com/nikkiii/status">Nikki's status page</a>.</p>
-                        </div>
-                </div>
-
-        </body>
-</html>
